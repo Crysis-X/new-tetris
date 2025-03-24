23d8d85074d323da28538c179b9f00f0e6cb7291
Crysis-X <spidrunner2008@gmail.com>
2025-03-24 18:47:13 +0300

    created tetris

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,35 @@
-export default function App(){
-  return <>Goodbye World</>
+import { useState, useEffect, useRef } from "react";
+import { Tetris, Board } from "./modules/Tetris";
+
+const WIDTH = 10;
+const HEIGHT = 20;
+export default function TetrisGame() {
+  const [board, setBoard] = useState<Board>([]);
+  const game = useRef(new Tetris(WIDTH, HEIGHT, setBoard));
+  useEffect(() => {
+    game.current.start();
+    return () => game.current.stop();
+  }, [game]);
+
+  return (
+    <div className="w-screen h-screen flex flex-col items-center">
+    <p className="text-lg">{game.current.getScore()}</p>  
+    <div className="p-5" style={{width: WIDTH * 25 + "px", height: HEIGHT * 25 + "px"}}>
+      {board.map((row, rowIndex) => (
+        <div key={rowIndex} className="flex">
+          {row.map((cell, colIndex) => (
+            <div
+            key={colIndex}
+            className="w-[25px] h-[25px] border-1 border-black"
+            style={{
+              borderColor: cell ? "gray" : "black",
+              backgroundColor: cell ? "black" : "white",
+            }}
+            />
+          ))}
+        </div>
+      ))}
+    </div>
+    </div>
+  );
 }
---
+++ src/modules/Tetris.ts
@@ -0,0 +1,155 @@
+const FIGURES = [
+  [[1, 1, 1, 1]], // I
+  [
+    [1, 1],
+    [1, 1],
+  ], // O
+  [
+    [0, 1, 0],
+    [1, 1, 1],
+  ], // T
+  [
+    [1, 1, 0],
+    [0, 1, 1],
+  ], // S
+  [
+    [0, 1, 1],
+    [1, 1, 0],
+  ], // Z
+  [
+    [1, 1, 1],
+    [1, 0, 0],
+  ], // L
+  [
+    [1, 1, 1],
+    [0, 0, 1],
+  ], // J
+];
+
+export type Board = number[][];
+
+export class Tetris {
+  private board: Board;
+  private score: number = 0;
+  private update: (board: Board) => void;
+  private figure: { figure: number[][]; x: number; y: number };
+  private intervalId: number | null = null;
+  private w: number;
+  private h: number;
+  constructor(w: number, h: number, update: (board: Board) => void) {
+    this.w = w;
+    this.h = h;
+    this.board = Array.from({ length: this.h }, () => Array(this.w).fill(0));
+    this.update = update;
+    this.figure = this.spawn();
+  }
+
+  start() {
+    this.updateGameState();
+    this.intervalId = window.setInterval(() => this.fall(), 500);
+    window.addEventListener("keydown", this.handle);
+  }
+
+  stop() {
+    if (this.intervalId !== null) {
+      clearInterval(this.intervalId);
+      this.intervalId = null;
+    }
+    window.removeEventListener("keydown", this.handle);
+  }
+  getScore = () => this.score;
+  private spawn() {
+    const figure = FIGURES[Math.floor(Math.random() * FIGURES.length)];
+    return { figure, x: Math.floor(this.w / 2) - 1, y: 0 };
+  }
+
+  private handle = (event: KeyboardEvent) => {
+    switch (event.key) {
+      case "ArrowLeft":
+        this.move(-1);
+        break;
+      case "ArrowRight":
+        this.move(1);
+        break;
+      case "ArrowDown":
+        this.fall();
+        break;
+      case "ArrowUp":
+        this.rotate();
+        break;
+    }
+  };
+  private fall() {
+    if (!this.isCollision(this.figure.figure, this.figure.x, this.figure.y + 1)) {
+      this.figure.y++;
+    } else {
+      this.lockFigure();
+      this.figure = this.spawn();
+      this.clearFullLines();
+    }
+    this.updateGameState();
+  }
+
+  private move(direction: number) {
+    if (!this.isCollision(this.figure.figure, this.figure.x + direction, this.figure.y)) {
+      this.figure.x += direction;
+      this.updateGameState();
+    }
+  }
+
+  private rotate() {
+    const rotated = this.figure.figure[0].map((_, i) =>
+      this.figure.figure.map(line => line[i]).reverse()
+    );
+
+    if (!this.isCollision(rotated, this.figure.x, this.figure.y)) {
+      this.figure.figure = rotated;
+      this.updateGameState();
+    }
+  }
+
+  private isCollision(figure: number[][], x: number, y: number): boolean {
+    return figure.some((line, rowIndex) =>
+      line.some(
+        (cell, colIndex) =>
+          cell &&
+          (x + colIndex < 0 ||
+            x + colIndex >= this.w ||
+            y + rowIndex >= this.h ||
+            this.board[y + rowIndex]?.[x + colIndex])
+      )
+    );
+  }
+
+  private lockFigure() {
+    this.figure.figure.forEach((line, rowIndex) =>
+      line.forEach((cell, colIndex) => {
+        if (cell) {
+          this.board[this.figure.y + rowIndex][this.figure.x + colIndex] = 1;
+        }
+      })
+    );
+  }
+
+  private clearFullLines() {
+    this.board = this.board.filter(line => line.includes(0));
+    this.score += (this.h - this.board.length) * 100;
+    while (this.board.length < this.h) {
+      this.board.unshift(Array(this.w).fill(0));
+    }
+  }
+
+  private updateGameState() {
+    const displayBoard = this.board.map(line => [...line]);
+
+    this.figure.figure.forEach((line, rowIndex) =>
+      line.forEach((cell, colIndex) => {
+        if (cell) {
+          displayBoard[this.figure.y + rowIndex][this.figure.x + colIndex] = cell;
+        }
+      })
+    );
+
+    this.update(displayBoard);
+  }
+}
